de33675b7d0577c40dc11eff3f0ef8d76b16e94a
khanreformiqo <momdou.khan@reformiqo.com>
2025-06-15 16:44:24 +0000

    update

--- wifi_ticket/www/ticket/buy/index.py
+++ wifi_ticket/www/ticket/buy/index.py
@@ -95,8 +95,8 @@ def buy_ticket():
 @frappe.whitelist(allow_guest=True)
 def initialize_payment_for_event(amount, reference_id):
     try:
-        success_url = f"https://wifi.jokoor.com/ticket/buy/success?ref={reference_id}"
-        error_url = f"https://wifi.jokoor.com/ticket/buy/error?ref={reference_id}"
+        success_url = f"https://www.ceesay.net/ticket/buy/success?ref={reference_id}"
+        error_url = f"https://www.ceesay.net/ticket/buy/error?ref={reference_id}"
         response = initialize_payment(amount, reference_id, success_url, error_url)
         return response
 
